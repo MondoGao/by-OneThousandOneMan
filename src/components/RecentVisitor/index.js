0b003b9fa54fe8d75b6fa0657763c238e38e6e68
Mondo <mondogao@gmail.com>
2017-04-18 19:12:10 +0800

    feat(components/RecentVisitor): 调整多数量时的样式

--- src/components/RecentVisitor/index.js
+++ src/components/RecentVisitor/index.js
@@ -5,10 +5,10 @@ import styles from './RecentVisitor.scss'
 import TitleOrnament from 'components/TitleOrnament'
 import UserAvatar from 'components/UserAvatar'
 
-const RecentVisitor = ({ visitorAvatarSrcs = [require('assets/airship@2x.png')] }) => {
+const RecentVisitor = ({ visitorAvatarSrcs = [require('assets/airship@2x.png'), require('assets/airship@2x.png'),require('assets/airship@2x.png'),require('assets/airship@2x.png'),require('assets/airship@2x.png'),require('assets/airship@2x.png')] }) => {
   const hasVisitor = visitorAvatarSrcs.length > 0
   const visitorAvatars = visitorAvatarSrcs.map(src => (
-    <UserAvatar src={src} blurLevel={6 - visitorAvatarSrcs.length}/>
+    <UserAvatar className={styles['avatar']} src={src} blurLevel={6 - visitorAvatarSrcs.length}/>
   ))
   
   return (
@@ -18,7 +18,9 @@ const RecentVisitor = ({ visitorAvatarSrcs = [require('assets/airship@2x.png')] 
         <span className={styles['header-text']}>最近访客</span>
         <TitleOrnament isReversed/>
       </h4>
-      {visitorAvatars}
+      <div className={styles['avatar-list']}>
+        {visitorAvatars}
+      </div>
       {hasVisitor ?
         <span className={styles['blur-tip']}>来访的朋友越多 访客头像越清晰</span> :
       <span className={styles['empty-tip']}>呜呜呜！暂无访客</span>}
