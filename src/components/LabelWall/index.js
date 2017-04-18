9317eea79632c5519276c4d93c798ea1f4b83f66
Mondo <mondogao@gmail.com>
2017-04-18 22:10:25 +0800

    feat(components/LabelWall): 增加动态弹幕

--- src/components/LabelWall/index.js
+++ src/components/LabelWall/index.js
@@ -3,21 +3,91 @@ import React from 'react'
 import styles from './LabelWall.scss'
 
 class LabelWall extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      existLabels: [],
+      labelPointer: 0,
+      addTimer: null,
+      clearTimers: []
+    }
+  }
+  
+  createLabel = () => {
+    clearTimeout(this.state.addTimer)
+    
+    const labelPointer = this.state.labelPointer++
+    
+    if (this.state.labelPointer >= this.props.labelIds.length) {
+      this.setState({
+        labelPointer: 0
+      })
+    }
+    
+    const top = Math.random() * 320 + 33
+  
+    let timer = setTimeout(() => {
+      clearTimeout(timer)
+    
+      this.setState({
+        existLabels: this.state.existLabels.filter(existLabel => existLabel !== label),
+        clearTimers: this.state.clearTimers.filter(clearTimer => clearTimer !== timer)
+      })
+    }, 5000)
+    
+    let label = <span key={timer} className={styles['label']} style={{top}}>
+      {this.props.labelIds[labelPointer]}
+    </span>
+
+    
+    this.setState({
+      existLabels: [
+        ...this.state.existLabels,
+        label
+      ],
+      clearTimers: [
+        ...this.state.clearTimers,
+        timer
+      ]
+    })
+    
+    this.setState({
+      addTimer: setTimeout(this.createLabel, Math.random() * 500 + 500)
+    })
+  }
+  
   render() {
     const hasLabel = this.props.labelIds.length > 0
     
     return (
       <div className={styles['wall-container']}>
         <div className={`${styles['wall-content']} ${hasLabel ? '' : styles['empty']}`}>
-        
+          {this.state.existLabels}
         </div>
       </div>
     )
   }
+  
+  componentDidMount() {
+    this.setState({
+      addTimer: setTimeout(this.createLabel, Math.random() * 500 + 500)
+    })
+  }
+  
+  componentWillUnmount() {
+    clearTimeout(this.state.addTimer)
+  }
+}
+
+const Label = ({ text }) => {
+  return (
+    <span className={styles['label']}>{text}</span>
+  )
 }
 
 LabelWall.defaultProps = {
-  labelIds: []
+  labelIds: ['要求太高了', '心疼我家的傻儿子！！！', '没有选择我', '单身2017', '等着我脱单后你再脱']
 }
 
 export default LabelWall
