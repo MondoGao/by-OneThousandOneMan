3087cb898bc0739a72567645ca2842f4160bfe9b
Mondo <mondogao@gmail.com>
2017-04-20 09:33:27 +0800

    feat(components/LabelInput): 增加可选标签的样式

--- src/components/LabelInput/index.js
+++ src/components/LabelInput/index.js
@@ -4,7 +4,22 @@ import styles from './LabelInput.scss'
 
 const LabelInput = () => {
   return (
-    <div></div>
+    <div className={styles['label-input-container']}>
+      <p className={styles['alter-label-container']}>
+        <AlternativeLabel>家穷人丑</AlternativeLabel>
+        <AlternativeLabel>要求太高</AlternativeLabel>
+        <AlternativeLabel>没有选择我</AlternativeLabel>
+        <AlternativeLabel>高冷</AlternativeLabel>
+      </p>
+    </div>
+  )
+}
+
+const AlternativeLabel = ({ animationDelay, children }) => {
+  return (
+    <span className={styles['alter-label']}>
+      {children}
+    </span>
   )
 }
 
