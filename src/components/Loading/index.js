31d4a36ad775e425c43c00f4311ad4fdf257fb89
Mondo <mondogao@gmail.com>
2017-04-20 21:32:45 +0800

    feat(components/Loading): 修改手势，增加动态省略号

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,27 +1,64 @@
 import React from 'react'
 
 import styles from './Loading.scss'
-import hand from 'assets/gesture@2x.png'
 
-const Loading = () => {
-  return (
-    <div className={styles['loading']}>
-      <section className={styles['heart-wrapper']}>
-        <FloatHeart animationDelay={100}/>
-        <FloatHeart animationDelay={750}/>
-        <FloatHeart animationDelay={400}/>
-      </section>
-      <section>
-        <img
-          src={hand}
-          alt="华科脱单"
-          className={styles['hand']}/>
-        <p className={styles['tip']}>
-          「华科脱单」玩命加载中...
-        </p>
-      </section>
-    </div>
-  )
+class Loading extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      isRock: false,
+      ellipsisNum: 0,
+      timer: null
+    }
+  }
+  
+  handleClick = (e) => {
+    this.setState(prevState => ({
+      isRock: !prevState.isRock
+    }))
+  }
+  
+  toggleLoadingEllipsis = () => {
+    this.setState(prevState => ({
+      ellipsisNum: (prevState.ellipsisNum + 1) % 4
+    }))
+  }
+  
+  render() {
+    let ellipsis = ''
+    for (let i = 0; i < this.state.ellipsisNum; i++) {
+      ellipsis += '.'
+    }
+    
+    return (
+      <div className={styles['loading']}>
+        <section className={styles['heart-wrapper']}>
+          <FloatHeart animationDelay={100}/>
+          <FloatHeart animationDelay={750}/>
+          <FloatHeart animationDelay={400}/>
+        </section>
+        <section>
+        <span
+          className={`${styles['hand']} ${this.state.isRock ? styles['rock'] : ''}`}
+          onClick={this.handleClick}/>
+          <p className={styles['tip']}>
+            「华科脱单」玩命加载中{ellipsis}
+          </p>
+        </section>
+      </div>
+    )
+  }
+  
+  componentDidMount() {
+    this.setState({
+      timer: setInterval(this.toggleLoadingEllipsis, 300)
+    })
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.state.timer)
+  }
 }
 
 const FloatHeart = ({ animationDelay = 0 }) => {
