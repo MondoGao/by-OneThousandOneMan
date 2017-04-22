5d411079e00c4930348cbd215f273b22256d2ade
Mondo <mondogao@gmail.com>
2017-04-22 23:31:07 +0800

    feat(components/LabelInput): 增加组件控制状态和待选标签单击动作

--- src/components/LabelInput/index.js
+++ src/components/LabelInput/index.js
@@ -2,26 +2,57 @@ import React from 'react'
 
 import styles from './LabelInput.scss'
 
-const LabelInput = () => {
-  return (
-    <div className={styles['label-input-container']}>
-      <p className={styles['alter-label-container']}>
-        <AlternativeLabel>家穷人丑</AlternativeLabel>
-        <AlternativeLabel>要求太高</AlternativeLabel>
-        <AlternativeLabel>没有选择我</AlternativeLabel>
-        <AlternativeLabel>高冷</AlternativeLabel>
-      </p>
-      <div className={styles['input-wrapper']}>
-        <input className={styles['label-input']} type="text"/>
-        <span/>
+class LabelInput extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      inputValue: ''
+    }
+  }
+  
+  handleChange = e => {
+    if (e.target.value.length < 11) {
+      this.setState({
+        inputValue: e.target.value
+      })
+    }
+  }
+  
+  handleLabelClick = e => {
+    this.setState({
+      inputValue: e.target.innerText
+    })
+  }
+  
+  render() {
+    const alterLabelTexts = ['家穷人丑', '要求太高', '没有选择我', '高冷']
+    const alterLabels = alterLabelTexts.map((text, input) => (
+      <AlternativeLabel onClick={this.handleLabelClick} key={input}>{text}</AlternativeLabel>
+    ))
+    
+    return (
+      <div className={styles['label-input-container']}>
+        <p className={styles['alter-label-container']}>
+          {alterLabels}
+        </p>
+        <div className={styles['input-wrapper']}>
+          <input
+            className={styles['label-input']}
+            type="text"
+            placeholder="来啊！输入标签啊！"
+            value={this.state.inputValue}
+            onChange={this.handleChange}/>
+          <span/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-const AlternativeLabel = ({ animationDelay, children }) => {
+const AlternativeLabel = ({ animationDelay, children, onClick }) => {
   return (
-    <span className={styles['alter-label']}>
+    <span className={styles['alter-label']} onClick={onClick}>
       {children}
     </span>
   )
