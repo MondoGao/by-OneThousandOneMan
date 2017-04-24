6e1c4e192cc897ada016bf7a4b4a81d2dc7a15a7
Mondo <mondogao@gmail.com>
2017-04-24 11:43:07 +0800

    chore(gulp): 修复预加载列表生成的依赖

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const Webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const Gulp = require('gulp');
-const GulpClean = require('gulp-clean');
+const Webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const Gulp = require('gulp')
+const GulpClean = require('gulp-clean')
 const fs = require('fs')
 const { resolve } = require('path')
 
-const prodConfig = require('./build/webpack.prod.config');
-const devConfig = require('./build/webpack.dev.config');
+const prodConfig = require('./build/webpack.prod.config')
+const devConfig = require('./build/webpack.dev.config')
 
-const utils = require('./build/utils');
+const utils = require('./build/utils')
 
 Gulp.task('clean', () =>
   Gulp.src('./dist', {read: false})
     .pipe(GulpClean())
-);
+)
 
-Gulp.task('generate:assetsList', () => {
+Gulp.task('generate:assetsList', () => (
   fs.readdir(resolve(__dirname, 'src', 'assets'), (err, files) => {
     fs.open(resolve(__dirname, 'src', 'assets', 'loadingList.js'), 'w+', (err, file) => {
       let list = []
@@ -30,14 +30,14 @@ Gulp.task('generate:assetsList', () => {
       fs.writeSync(file, conclution)
     })
   })
+))
+
+Gulp.task('build', ['clean', 'generate:assetsList'], () => {
+  utils.build(Webpack, prodConfig)
 })
 
-Gulp.task('build', ['clean'], function () {
-  utils.build(Webpack, prodConfig);
-});
+Gulp.task('serve', ['generate:assetsList'], () => {
+  utils.serve(Webpack, WebpackDevServer, devConfig)
+})
 
-Gulp.task('serve', function () {
-  utils.serve(Webpack, WebpackDevServer, devConfig);
-});
-
-Gulp.task('default', ['serve']);
+Gulp.task('default', ['serve'])
