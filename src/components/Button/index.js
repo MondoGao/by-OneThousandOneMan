c528f9403e6dcb76f38b0b1d390e291eef5def72
Mondo <mondogao@gmail.com>
2017-04-25 21:34:02 +0800

    feat(components/Button): 增加脱单秘籍访问公众号链接

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import styles from './Button.scss'
+import settings from 'sources/settings'
 
 const Button = ({ children, type = 'normal', className = '', onClick = null }) => {
   return (
@@ -8,6 +9,7 @@ const Button = ({ children, type = 'normal', className = '', onClick = null }) =
       className={`${styles['button']} ${styles[type]} ${className}`}
       onClick={onClick}>
       {children}
+      {type === 'normal' ? <a className={styles['cheat-link']} href={settings.wechatHref}/> : null}
       </span>
   )
 }
---
+++ src/sources/settings.js
@@ -0,0 +1,3 @@
+export default {
+  wechatHref: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzI1NzA3NTkwNw==&scene=110#wechat_redirect'
+}
