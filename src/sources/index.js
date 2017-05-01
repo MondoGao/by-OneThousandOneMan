811b680f72a70f4da7ac89ca4c8b7703c8d18e34
Mondo <mondogao@gmail.com>
2017-05-01 17:38:55 +0800

    chore(sources/settings): 修改 appid

--- src/sources/index.js
+++ src/sources/index.js
@@ -2,7 +2,8 @@ export * from 'sources/users'
 import { checkStatus } from 'sources/utils'
 
 export const settings = {
-  appId: 'wxb8c111df49f72f25',
+  // appId: 'wxb8c111df49f72f25',
+  appId: 'wx690cda5c2ea251b2',
   scope: 'snsapi_userinfo',
   wechatHref: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzI1NzA3NTkwNw==&scene=110#wechat_redirect',
   publicPath: '/single/'
