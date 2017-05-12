58e920549149cf04ba7fb5ddef2125b59c3c5511
Mondo <mondogao@gmail.com>
2017-05-12 19:31:37 +0800

    refactor: 匹配多页逻辑

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import * as consts from 'actions/consts'
 
-const isLoading = (state = !localStorage.getItem('myselfId'), action) => {
+const isLoading = (state = true, action) => {
   switch (action.type) {
     case consts.TOGGLE_LOADING:
       return action.payload
