068c03533f6811b9569187abbec7834977eebede
Mondo <mondogao@gmail.com>
2017-05-14 10:07:07 +0800

    feat: 增加四处动作埋点

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,13 +3,18 @@ import React from 'react'
 import styles from './Button.scss'
 import { settings } from 'sources'
 
-const Button = ({ children, type = 'normal', className = '', onClick = null }) => {
+const Button = ({ children, type = 'normal', className = '', onClick = null, onCheatClick = null }) => {
   return (
     <span className={`${styles['button']} ${styles[type]} ${className}`}>
       <span className={styles['text']} onClick={onClick}>
         {children}
       </span>
-      {type === 'normal' ? <a className={styles['cheat-link']} href={settings.wechatHref}/> : null}
+      {type === 'normal' ?
+        <a
+          className={styles['cheat-link']}
+          href={settings.wechatHref}
+          onClick={onCheatClick}/>
+        : null}
       </span>
   )
 }
