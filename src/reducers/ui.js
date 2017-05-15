b4c057f00c10aad98c568bba8a142091ffe652d9
Mondo <mondogao@gmail.com>
2017-05-15 23:25:13 +0800

    fix: 修复发送按钮问题和改变加载图标显示逻辑

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -4,6 +4,8 @@ import * as consts from 'actions/consts'
 const isLoading = (state = true, action) => {
   switch (action.type) {
     case consts.TOGGLE_LOADING:
+      sessionStorage.setItem('isFirstLoading', 'huaketuodan')
+  
       return action.payload
     default:
       return state
